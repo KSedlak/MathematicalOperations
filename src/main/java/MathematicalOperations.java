433c776dcb5d82f4d17cf00b0028361e042db45b
KSedlak <krzysztof.sedlak@hotmail.com>
2015-08-21 10:14:26 +0200

    Merge branch 'master' into pr/n2_pawel-szlagor
    Conflicts:
    	src/main/java/MathematicalOperations.java

--- src/main/java/MathematicalOperations.java
+++ src/main/java/MathematicalOperations.java
@@ -1,8 +1,18 @@
 import java.math.RoundingMode;
 
 import com.google.common.math.*;
-
 public class MathematicalOperations {
+	
+	
+	
+	
+
+	/**
+	 * @throws ArithmeticException if the result overflows the {@link Integer#MAX_VALUE}
+	 */
+	public int add(int a, int b) throws ArithmeticException {
+		return IntMath.checkedAdd(a, b);
+	}
 
 	public double division(int a, int b, boolean round) throws Exception {
 		if (round)
--- src/test/java/MathematicalOperationsTest.java
+++ src/test/java/MathematicalOperationsTest.java
@@ -1,7 +1,21 @@
-import org.junit.Assert;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class MathematicalOperationsTest {
+
+	@Test
+	public void testAddNormal() {
+		MathematicalOperations mo = new MathematicalOperations();
+		assertEquals(mo.add(1, 2), 3);
+	}
+
+	@Test(expected = ArithmeticException.class)
+	public void testAddOverflow() {
+		new MathematicalOperations().add(Integer.MAX_VALUE, 1);
+	}
 
 	@Test
 	public void testDivisionNormal() throws Exception {
@@ -10,7 +24,7 @@ public class MathematicalOperationsTest {
 		// when
 		double result = mo.division(20, 5, false);
 		// then
-		Assert.assertEquals(4.0, result);
+		Assert.assertEquals(4.0, result,0);
 	}
 
 	@Test
@@ -29,13 +43,13 @@ public class MathematicalOperationsTest {
 	}
 
 	@Test
-	public void testDivisionRonding() throws Exception {
+	public void testDivisionRuonding() throws Exception {
 		// given
 		MathematicalOperations mo = new MathematicalOperations();
 		// when
 		double result = mo.division(20, 3, true);
 		// then
-		Assert.assertEquals(6.0, result);
+		Assert.assertEquals(6.0, result,0);
 	}
 	
 	
